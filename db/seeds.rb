bcf11cef26ddf03bdb0e3a4aa91147bbf4f6d6a7
Michael Alexander <alexandermw@gmail.com>
2014-10-04 11:36:11 -0700

    much better seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,236 +2,160 @@
 
 # Make sure your seeds file blows away any existing models every time it starts running: JUST RUN THIS WITH RAKE db:reset
 
+#scalar 10 => about 1,000 Cities, 1,000 Users, <1,000 Orders
 
-
-
-
-
-# Make everything dynamic so you can just specify (hard code) a single "seed multiplier", e.g. 1 or 4 or 20 and it will scale up the size of all your seeds by that factor. This can be useful if you want to start with a very small seed but then ramp up to a huge database for performance testing.
-
-SEED_MULTIPLIER = 100
-
-
-
-
-# Have at least 100 users. Stagger their join dates any time in the last year. Show growth in the rate of user signups over time.
-0.upto(100 * SEED_MULTIPLIER) do |number|
-  u = User.new
-  u.id = number
-  u.first_name = Faker::Name.first_name
-  u.last_name = Faker::Name.last_name
-  u.email = Faker::Internet.email
-  u.save
+SCALAR = 10 # Seed multiplier; caution: exponential data increase
+ 
+#generate products
+sample_categories = []
+ 
+SCALAR.times do
+  sample_categories << Faker::Commerce.department
 end
-
-
-
-# Have at least 100 cities, some associated with addresses and others not. Try to clump addresses into cities so some cities will contain many users/orders.
-
-num_cities = 100 * SEED_MULTIPLIER
-
-0.upto(num_cities) do |number|
-  c = City.new
-  c.id = number
-  c.name = Faker::Address.city
-  c.save
+ 
+sample_categories.each do |name|
+  category = Category.new()
+  category[:name]        = name
+  category[:description] = Faker::Lorem.sentence
+  category.save
 end
-
-
-
-# Have 5-10 states. You don't need to fluctuate this with your "seed multiplier" below.
-
-0.upto(5) do |i|
-  s = State.new
-  s.name = Faker::Address.state
-  s.save
+ 
+(SCALAR**2).times do
+  p = Product.new()
+  p[:name]        = Faker::Commerce.product_name
+  p[:category_id] = rand(Category.count)+1
+  p[:description] = Faker::Lorem.sentence
+  p[:sku]         = (Faker::Code.ean).to_i
+  p[:price]       = Faker::Commerce.price
+  p.save
 end
-
-
-
-# users should have any number from 0 to 5 addresses listed, as well as one designated the default "shipping" and one the default "billing" (can be the same). Each order has to have an address assigned for "shipping" and "billing", so these defaults are autopopulated if the user doesn't override as part of the checkout process (like Amazon.com). The other addresses are just there as potential options for future orders.
-
-#need to iterate through users via #pop
-u = User.all.to_a
-
-#even split among 0..5 addresses
-0.upto(5) do |num_addresses|
-
-  #make that certain number of addresses for 1/6 of the users
-  (u.size / 6).times do
-
-    #for an individual user, make them num_addresses addresses
-    num_addresses.times do
-      a = Address.new
-      a.street_address = Faker::Address.street_address
-      a.secondary_address = Faker::Address.secondary_address
-      a.zip_code = Faker::Address.zip_code
-      a.phone_number = Faker::PhoneNumber.phone_number
-      a.city = 0 #setting these later
-      a.state = 0 #setting these later
-      a.user_id = u.last.id #the user on top of the stack
-      a.save
-    end #num_addresses.times
-
-    u.pop #make way for the next user
-
-  end #u.size / 6.times
-
-end #0.upto(5)
-
-
-
-
-#create 50/25/12/6/3/2/1 split for cities, states
-
-percent = 50
-id = 1
-
-while percent > 1 do
-  (percent * SEED_MULTIPLIER).times do
-    a = Address.find_by_city(0)
-    a.city = id
-    a.state = id
-    a.save
-  end
-
-  #iterate both the percentage and the city/state id
-  percent /= 2
-  id += 1
+ 
+#generate addresses, SCALAR*111 cities
+states =
+["Alabama", "Alaska", "Arizona", "Arkansas", "California",
+"Colorado", "Connecticut", "Delaware", "Florida", "Georgia",
+"Hawaii", "Idaho", "Illinois", "Indiana", "Iowa",
+"Kansas", "Kentucky", "Louisiana", "Maine", "Maryland",
+"Massachusetts", "Michigan", "Minnesota", "Mississippi", "Missouri",
+"Montana", "Nebraska", "Nevada", "New Hampshire", "New Jersey",
+"New Mexico", "New York", "North Carolina", "North Dakota", "Ohio",
+"Oklahoma", "Oregon", "Pennsylvania", "Rhode Island", "South Carolina",
+"South Dakota", "Tennessee", "Texas", "Utah", "Vermont",
+"Virginia", "Washington", "West Virginia", "Wisconsin", "Wyoming"]
+ 
+states.each do |state|
+  State.new({:name => state})
 end
-
-#for iterating
-users = User.all.to_a
-
-#find all of each user's addresses and set billing/shipping defaults
-users.each do |user|
-  u_addresses = Address.where(:user_id => user.id)
-
-  if u_addresses.any?
-    user.billing_id = u_addresses.first.id
-    user.shipping_id = u_addresses.last.id
-    user.save
-  else
-    #skip it to avoid errors
-  end
+ 
+def sample_city # generates a new city/state/zip combination
+  city = City.new({:name => Faker::Address.city})
+  city.save
+  {"city" => city.id, "state" => (rand(50)+1), "zip" => (Faker::Address.zip_code).to_i}
 end
-
-
-
-# Have 10-30 products across a half-dozen-or-so categories
-
-
-#sets everything but the category
-num_products = 20 * SEED_MULTIPLIER 
-
-0.upto(num_products-1) do |pid|
-  product = Product.new
-  product.id = pid
-  product.name = Faker::Commerce.product_name
-  product.price = Faker::Commerce.price
-  product.sku = Faker::Code.ean
-  product.description = Faker::Lorem.paragraph
-  product.category_id = 0
-  product.save
-end
-
-#creates the categories
-num_categories = 6 * SEED_MULTIPLIER
-
-0.upto(num_categories-1) do |cid|
-  c = Category.new
-  c.id = cid
-  c.name = Faker::Commerce.department(2)
-  c.description = Faker::Lorem.paragraph
-  c.save
-
-  #1/6 of products set to each category
-  (num_products / 6).times do
-    product = Product.find_by_category_id(0)
-    if product
-      product.category_id = cid
-      product.save
+ 
+def sample_cities # appends city/state/zip combinations to three echelons in an array, with more instances for each successive echelon
+  cities, towns, villages = [], [], []
+  SCALAR.times do
+    cities << sample_city
+    10.times do
+      towns << sample_city
+      10.times do
+        villages << sample_city
+      end
     end
   end
-
+  [cities, towns, villages]
 end
-
-
-
-
-
-# populate a historical record of at least 100 orders staggered throughout the past year. Show growth in the rate of orders over time.
-#populate at least 25 shopping carts
-
-num_orders = 125 * SEED_MULTIPLIER
-
-0.upto(num_orders-1) do |order_id|
-  o = Order.new
-  o.userid = 0 #will change outside this loop
-
-  #shopping carts are just orders not yet checked out
-  o.checked_out = false if order_id < ( num_orders / 5 )
-
-  o.save
+ 
+SAMPLECITIES = sample_cities # so that the method is only run once
+ 
+def random_user_address(user_id)
+  address_choices = (Address.select(:id).where(:user_id => user_id)).to_a
+  address_choices[0] ? address_choices.sample[:id] : nil
 end
-
- #minutes in a year / num_orders =even distribution
-mins = 525949 / num_orders
-
-orders = Order.all
-
-#show growth in the rate of orders over time
-orders.each do |order|
-  order.checkout_date = Time.now - 1.year + mins.minutes
-  order.save
-
-  #every time this iterates, date offset moves closer to the present
-  #curve moves slowly, more slowly the more records there are
-  mins = mins
+ 
+def city_sampling
+  SAMPLECITIES.sample.sample
 end
-
-
-
-u = User.all
-
-#loop through all the shopping carts unassigned
-user_index = 0
-while Order.find_by(:checked_out => false, :userid => 0)
-  o = Order.find_by(:checked_out => false, :userid => 0)
-  o.userid = user_index
-  o.save
-  user_index += 1 #make sure no user gets 2 carts
-end
-
-user_index = 0
-#loop through all orders unassigned to a user
-while Order.find_by(:checked_out => true, :userid => 0)
-  order = Order.find_by(:checked_out => true, :userid => 0)
-  
-  #if the user with specified index has a shipping and billing address entered,
-  #claim it for this order
-  if u[user_index].shipping_id && u[user_index].billing_id
-    order.userid = user_index
-    order.shipping_id = u[user_index].shipping_id
-    order.billing_id = u[user_index].billing_id
-    order.save
-  end
-  
-  user_index += 2 #iterate through users so only some have carts AND orders
-  user_index = 0 if user_index >= u.size
-end
-
-
-#create 3 times as many purchases as orders
-0.upto(orders.size) do |order_id|
-  3.times do 
-    purchase = Purchase.new
-    
-    purchase.order_id = order_id
-    purchase.product_id = order_id % num_products #unpredictable, not random, spreads across all products
-    purchase.quantity = (order_id % 10) + 1  #unpredictable, but not random, and 1 - 10
-    purchase.save
+ 
+def generate_addresses(user_id)
+  (rand(6)).times do
+    city_instance = city_sampling
+    a = Address.new()
+    a[:user_id]        = user_id
+    a[:street_address] = Faker::Address.street_address
+    a[:city]           = city_instance["city"]
+    a[:state]          = city_instance["state"]
+    a[:zip_code]       = city_instance["zip"]
+    a[:phone_number]   = Faker::PhoneNumber.phone_number
+    a.save
   end
 end
-
-
+ 
+#generate users, SCALAR**2 users
+def creation_date
+  time_frames = []
+  SCALAR.times do |x|
+    time_frames << Time.now - ((x+1)*3).month
+  end
+  date_range = (time_frames.sample..Time.now)
+  rand(date_range)
+end
+ 
+(SCALAR**3).times do |x|
+  sample_name = [Faker::Name.first_name, Faker::Name.last_name]
+  generate_addresses(x+1)
+ 
+  u = User.new()
+  u[:first_name]  = sample_name[0]
+  u[:last_name]   = sample_name[1]
+  u[:email]       = Faker::Internet.email(sample_name.join(" "))
+  u[:billing_id]  = random_user_address(x+1)
+  u[:shipping_id] = random_user_address(x+1)
+  u[:created_at]  = creation_date
+ 
+  u.save
+end
+ 
+#generate order contents
+def generate_contents(order_id)
+  (rand(SCALAR)+1).times do
+    c = Purchase.new()
+    c[:order_id]   = order_id
+    c[:product_id] = rand(Product.count)+1
+    c[:quantity]   = rand(SCALAR)+1
+    c.save
+  end
+end
+ 
+#generate orders
+def no_cart?(user_id)
+  Order.where(:checked_out => false, :userid => user_id).empty?
+end
+ 
+def completion(user)
+  if no_cart?(user[:id])
+    rand(5) > 0 && ! user[:billing_id].nil?
+  else
+    true
+  end
+end
+ 
+def placement_date(user)
+  rand(user[:created_at]..Time.now)
+end
+ 
+(SCALAR**3).times do
+  sample_user = User.find(rand(User.count)+1)
+  if sample_user[:billing_id] || no_cart?(sample_user[:id])
+    completed_order = completion(sample_user)
+    o = Order.new()
+    o[:userid]        = sample_user.id
+    o[:shipping_id]   = random_user_address(sample_user.id)
+    o[:billing_id]    = random_user_address(sample_user.id)
+    o[:checked_out]   = completed_order
+    o[:checkout_date] = placement_date(sample_user) if completed_order
+    o.save
+    generate_contents(o[:id])
+  end
+end
