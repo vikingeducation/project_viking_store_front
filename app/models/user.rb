4ad231dd2a965cf359d771be75a0a2adaa90a22b
Michael Alexander <alexandermw@gmail.com>
2014-10-04 21:06:12 -0700

    no real change

--- app/models/user.rb
+++ app/models/user.rb
@@ -21,7 +21,7 @@ class User < ActiveRecord::Base
   end
 
   def self.highest_lifetime
-    User.select("users.first_name AS user_first_name, users.last_name AS user_last_name, SUM(purchases.quantity * products.price) AS value").joins("JOIN orders ON users.id = orders.userid JOIN purchases ON orders.id = purchases.order_id JOIN products ON purchases.product_id = products.id").where("orders.checked_out" => true).group("users.id").order("value DESC").limit(1).first
+    User.select("users.first_name AS user_first_name, users.last_name AS user_last_name, (purchases.quantity * products.price) AS value").joins("JOIN orders ON users.id = orders.userid JOIN purchases ON orders.id = purchases.order_id JOIN products ON purchases.product_id = products.id").where("orders.checked_out" => true).group("users.id").order("value DESC").limit(1).first
   end
 
   def self.highest_average_order
