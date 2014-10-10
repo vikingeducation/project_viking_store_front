227de464fce44b6d657fe72444abf9a70c735dc8
Andur Carr <carr.andur@gmail.com>
2014-10-10 11:34:18 -0700

    Fix typo in address.rb

--- app/models/address.rb
+++ app/models/address.rb
@@ -6,7 +6,7 @@ class Address < ActiveRecord::Base
   has_one :default_billing, foreign_key: :billing_id, :class_name => "User"
   has_one :default_shipping, foreign_key: :shipping_id, :class_name => "User"
 
-  validates :stree_address,
+  validates :street_address,
             :zip_code,
             :city,
             :state,
