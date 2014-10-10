befe8133f9c7d9295e76d244d5ec10f60fd34b6d
Andur Carr <carr.andur@gmail.com>
2014-10-10 13:44:58 -0700

    Build user index page

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,5 @@
 module UsersHelper
+  def completed_orders(user)
+    user.orders.where(checked_out: true)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   root 'dashboard#index'
   resources :categories
   resources :products
+  resources :users
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
