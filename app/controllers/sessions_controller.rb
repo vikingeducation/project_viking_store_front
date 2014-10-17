be33fb8538613d0332d280402ef6b3d0aaa78aee
Andur Carr <carr.andur@gmail.com>
2014-10-17 13:14:37 -0700

    Improve sign in/out

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -16,36 +16,12 @@ class SessionsController < ApplicationController
 
   def destroy
     user = current_user
-    if sign_out(user)
+    if sign_out
       flash[:success] = "Signed out"
       redirect_to root_path
     else
       flash[:error] = "Failed to sign out"
       redirect_to root_path
     end
-  end
-
-  private
-
-  def sign_in(user)
-    session[:current_user_id] = user.id
-    current_user = user
-  end
-
-  def sign_out
-    session.delete(:current_user_id) && current_user = nil
-  end
-
-  def current_user
-    return nil unless session[:current_user_id]
-    @current_user ||= User.find(params[:current_user_id])
-  end
-
-  def current_user=(user)
-    @current_user = user
-  end
-
-  def signed_in_user?
-    @current_user.present?
   end
 end
