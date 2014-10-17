5e15a5f790dd913cdd987f1bb239059fe1111ce5
Andur Carr <carr.andur@gmail.com>
2014-10-16 22:24:52 -0700

    Maybe the session cart works

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,6 +2,20 @@ class ProductsController < ApplicationController
   def index
     filter = params[:products_filter]
     @products = filter.present? ? Product.where("category_id = ?", filter) : Product.all
+    if params[:item]
+      session[:cart] ||= {}
+      item = params[:item]
+      if Product.where("id = ?", item).present?
+        if session[:cart][item]
+          session[:cart][item] += 1
+        else
+          session[:cart][item] = 1
+        end
+        flash.now[:success] = "Item added to cart"
+      else
+        flash.now[:error] = "Product is invalid"
+      end
+    end
   end
 
   def show
