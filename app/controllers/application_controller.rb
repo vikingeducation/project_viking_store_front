7aa0c8257b976b52e7b02cf94cf8030f007d07a5
Andur Carr <carr.andur@gmail.com>
2014-10-17 19:20:43 -0700

    add helpers from app controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,10 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   before_action :current_user
+  helper_method :current_user
+  helper_method :signed_in_user?
+
+  private
 
   def sign_in(user)
     session[:current_user_id] = user.id
