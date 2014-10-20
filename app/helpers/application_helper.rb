bcfb9baffec288f3b13b3fe88ea61f42261566c5
Tom McLaughlin <tim5046@gmail.com>
2014-10-20 01:09:16 -0400

    shopping cart tweak

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,8 +1,12 @@
 module ApplicationHelper
 		include ActionView::Helpers::NumberHelper
 
-    def signed_in?(user)
-      false
 
+
+    def get_random_product_image
+    ['mom.jpg','meatballs.jpg','jam.jpg',
+      'god.jpg','glasses.jpg','fry.jpg',
+      'egg.jpg','cup.jpg','bacon.jpg',
+      'axe3.jpg','axe2.jpg','axe1.jpg','5hr.jpg'].sample
     end
 end
--- app/helpers/products_helper.rb
+++ app/helpers/products_helper.rb
@@ -1,8 +1,3 @@
 module ProductsHelper
-  def get_random_product_image
-    ['mom.jpg','meatballs.jpg','jam.jpg',
-      'god.jpg','glasses.jpg','fry.jpg',
-      'egg.jpg','cup.jpg','bacon.jpg',
-      'axe3.jpg','axe2.jpg','axe1.jpg','5hr.jpg'].sample
-  end
+
 end
